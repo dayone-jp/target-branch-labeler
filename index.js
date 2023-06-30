47924d58ad063a1cc1f981373948fb7135436cc0
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-06-30 10:08:49 +0900

    remove labels functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function addMergeBlockedLabelsToAllTargetPrs() {
   try {
     const develop = core.getInput("release-source-branch");
     const label = core.getInput("label");
+    const actionType = core.getInput("action-type");
 
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN ?? "").rest;
 
@@ -17,13 +18,25 @@ async function addMergeBlockedLabelsToAllTargetPrs() {
       });
 
       for (const pr of prs) {
-        await octokit.issues.addLabels({
-          ...github.context.repo,
-          issue_number: pr.number,
-          labels: [label],
-        });
+        if (actionType === "add") {
+          await octokit.issues.addLabels({
+            ...github.context.repo,
+            issue_number: pr.number,
+            labels: [label],
+          });
+        } else {
+          try {
+            await octokit.issues.removeLabel({
+              ...github.context.repo,
+              issue_number: pr.number,
+              name: label,
+            });
+          } catch (e) {
+            core.info(`No "${label}" label for PR #${pr.number}`);
+          }
+        }
 
-        core.info(`Added "${label}" label to PR #${pr.number}`);
+        core.info(`${actionType} "${label}" label to PR #${pr.number}`);
       }
     } catch (error) {
       core.error("Error adding label to PRs:", error);
